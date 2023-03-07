e1c0bf84b27c62aa82559eee4082653991707f30
JimHwkins <howlarrior@gmail.com>
2023-03-07 00:35:31 -0300

    Complete Exercise 7: Validations for both models

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,25 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+puts "Please enter a store name:"
+store_name = gets.chomp
+
+puts "Please enter the annual revenue:"
+annual_revenue = gets.chomp.to_i
+
+puts "Does the store carry men's apparel? (y/n)"
+mens_apparel = gets.chomp.downcase == 'y'
+
+puts "Does the store carry women's apparel? (y/n)"
+womens_apparel = gets.chomp.downcase == 'y'
+
+new_store = Store.new(name: store_name, annual_revenue: annual_revenue, mens_apparel: mens_apparel, womens_apparel: womens_apparel)
+
+if new_store.save
+  puts "New store successfully created"
+else
+  puts "The store couldn't be saved:"
+  new_store.errors.full_messages.each do |msg|
+    puts "- #{msg}"
+  end
+end
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,13 @@
 class Store < ActiveRecord::Base
   has_many :employees
+
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validate :must_have_mens_or_womens_apparel
+
+  def must_have_mens_or_womens_apparel
+    if !mens_apparel && !womens_apparel
+      errors.add(:base, "Store must carry at least one men's or women's apparel")
+    end
+  end
 end
